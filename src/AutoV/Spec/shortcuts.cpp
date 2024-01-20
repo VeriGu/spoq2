65b0d11b1ea08a5e6d4246dfc3b5453c8c4256b1
Wei Qiang <wq2167@columbia.edu>
2024-01-19 21:51:57 -0500

    fix shortcuts

--- src/AutoV/Spec/shortcuts.cpp
+++ src/AutoV/Spec/shortcuts.cpp
@@ -1,13 +1,13 @@
 #include <shortcuts.h>
 
 namespace autov {
-	static SpecNode* _Let(string name, SpecNode* val, SpecNode* body) {
+	SpecNode* _Let(string name, SpecNode* val, SpecNode* body) {
 		auto vec = new vector<unique_ptr<PatternMatch>>();
 		vec->push_back(unique_ptr<PatternMatch>(new PatternMatch(unique_ptr<SpecNode>(new Symbol(name)), unique_ptr<SpecNode>(body))));
 		return new Match(unique_ptr<SpecNode>(val), unique_ptr<vector<unique_ptr<PatternMatch>>>(vec));
 	}
 
-	static SpecNode* _When(SpecNode* pat, SpecNode* val, SpecNode* body) {
+	SpecNode* _When(SpecNode* pat, SpecNode* val, SpecNode* body) {
 		auto somepat = new vector<unique_ptr<SpecNode>>();
 		somepat->push_back(unique_ptr<SpecNode>(pat));
 		PatternMatch *some = new PatternMatch(unique_ptr<SpecNode>(new Expr("Some", unique_ptr<vector<unique_ptr<SpecNode>>>(somepat))), unique_ptr<SpecNode>(body));
@@ -19,29 +19,29 @@ namespace autov {
 		return new Match(unique_ptr<SpecNode>(val), unique_ptr<vector<unique_ptr<PatternMatch>>>(pats));
 	}
 
-	static SpecNode* _Some(SpecNode* val) {
+	SpecNode* _Some(SpecNode* val) {
 		auto vec = new vector<unique_ptr<SpecNode>>();
 		vec->push_back(unique_ptr<SpecNode>(val));
 		return new Expr("Some", unique_ptr<vector<unique_ptr<SpecNode>>>(vec));
 	}
 
-	static SpecNode* _Tuple(vector<unique_ptr<SpecNode>> *vec) {
+	SpecNode* _Tuple(vector<unique_ptr<SpecNode>> *vec) {
 		return new Expr("Tuple", unique_ptr<vector<unique_ptr<SpecNode>>>(vec));
 	}
 
-	static SpecNode* _List(vector<unique_ptr<SpecNode>> *vec) {
+	SpecNode* _List(vector<unique_ptr<SpecNode>> *vec) {
 		return new Expr("List", unique_ptr<vector<unique_ptr<SpecNode>>>(vec));
 	}
 
-	static SpecNode* _st(shared_ptr<SpecType> abs_data) {
+	SpecNode* _st(shared_ptr<SpecType> abs_data) {
 		return new Symbol("st", abs_data);
 	}
 
-	static SpecNode* _init_st(shared_ptr<SpecType> abs_data) {
+	SpecNode* _init_st(shared_ptr<SpecType> abs_data) {
 		return new Symbol("st", abs_data);
 	}
 
-	static SpecNode* _name(string name, unordered_map<string, shared_ptr<SpecType>> *types) {
+	SpecNode* _name(string name, unordered_map<string, shared_ptr<SpecType>> *types) {
 		if(types->find(name) != types->end()) {
 			return new Symbol(name, (*types)[name]);
 		} else {
