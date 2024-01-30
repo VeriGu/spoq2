768b2f24aee6457b31dcaf277813c8bc420fc2f3
XuhengLi <xl2784@columbia.edu>
2024-01-29 22:35:10 -0500

    Declaration of `parse_inline_asm`.

--- include/inline_asm.h
+++ include/inline_asm.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <llvm.h>
+#include <irtypes.h>
 
 namespace autov::IRLoader {
 using std::string;
@@ -13,4 +15,7 @@ public:
     string objdump;
     string coq;
 };
+
+IASM parse_inline_asm(string fname, string asm_text, shared_ptr<IRType> rettype, vector<unique_ptr<FuncArg>> &arglist, string constraints);
+
 } // namespace autov::IRLoader
