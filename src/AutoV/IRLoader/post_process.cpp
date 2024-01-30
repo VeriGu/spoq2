c0c443a852a089f86672cec0d3f2b8a73cde5b72
Fanqi Yu <fanqi.yu@columbia.edu>
2024-01-30 14:02:35 -0500

    Reduce tab size

--- src/AutoV/IRLoader/post_process.cpp
+++ src/AutoV/IRLoader/post_process.cpp
@@ -11,65 +11,65 @@ using namespace std::string_literals;
 namespace autov::IRLoader {
 
 void extract_inline_asm(shared_ptr<IRModule> mod) {
-        int iasm_count = 0;
+    int iasm_count = 0;
 
-        auto synthesize_coq_def = [](const string& fname, shared_ptr<IRType> rettype, vector<shared_ptr<FuncArg>> &args) {
-                return ""s;
+    auto synthesize_coq_def = [](const string& fname, shared_ptr<IRType> rettype, vector<shared_ptr<FuncArg>> &args) {
+        return ""s;
 
-                // if isinstance(rettype, TVoid):
-                //         coq_ret = "option RData"
-                // else:
-                //         coq_ret = f"option ({ir_type_to_spec(fname, rettype)} * RData)"
+        // if isinstance(rettype, TVoid):
+        //         coq_ret = "option RData"
+        // else:
+        //         coq_ret = f"option ({ir_type_to_spec(fname, rettype)} * RData)"
 
-                // if isinstance(rettype, TStruct):
-                //         st_str = f"Record st_{fname} := mkst_{fname} " + "{" + "}.\n"
-                // else:
-                //         st_str = ""
-                // return st_str + "Definition " + fname + "_spec " + \
-                //         " ".join([f"(arg{i}: {ir_type_to_spec(fname, arg.typ)})" for i, arg in enumerate(args)]) + \
-                //         " (st: RData) : " + coq_ret + " := None.\n"
-        };
+        // if isinstance(rettype, TStruct):
+        //         st_str = f"Record st_{fname} := mkst_{fname} " + "{" + "}.\n"
+        // else:
+        //         st_str = ""
+        // return st_str + "Definition " + fname + "_spec " + \
+        //         " ".join([f"(arg{i}: {ir_type_to_spec(fname, arg.typ)})" for i, arg in enumerate(args)]) + \
+        //         " (st: RData) : " + coq_ret + " := None.\n"
+    };
 
-        std::function<int(vector<unique_ptr<IRInst>>&)> find_inline_asm;
-        find_inline_asm = [&](vector<unique_ptr<IRInst>>& insts) {
-                int failed = 0;
+    std::function<int(vector<unique_ptr<IRInst>>&)> find_inline_asm;
+    find_inline_asm = [&](vector<unique_ptr<IRInst>>& insts) {
+        int failed = 0;
 
-                for (const auto& i : insts) {
-                        if (dynamic_cast<ICall*>(i.get())) {
-                                auto call_inst = dynamic_cast<ICall*>(i.get());
-                                string iasm_prefix;
+        for (const auto& i : insts) {
+            if (dynamic_cast<ICall*>(i.get())) {
+                auto call_inst = dynamic_cast<ICall*>(i.get());
+                string iasm_prefix;
 
-                                if (dynamic_cast<VInlineAsm*>(call_inst->func.get())) {
-                                        auto asm_func = dynamic_cast<VInlineAsm*>(call_inst->func.get());
-                                        string asm_text = asm_func->asm_string;
+                if (dynamic_cast<VInlineAsm*>(call_inst->func.get())) {
+                    auto asm_func = dynamic_cast<VInlineAsm*>(call_inst->func.get());
+                    string asm_text = asm_func->asm_string;
 
-                                        if (asm_text.find("smc") != string::npos) {
-                                                iasm_prefix = "smc_";
-                                        } else {
-                                                iasm_prefix = "";
-                                        }
+                    if (asm_text.find("smc") != string::npos) {
+                        iasm_prefix = "smc_";
+                    } else {
+                        iasm_prefix = "";
+                    }
 
-                                        string fname = "iasm_" + iasm_prefix + std::to_string(iasm_count);
-                                        string old_name = fname;
-                                        string constraints = asm_func->constraints;
-                                        shared_ptr<IRType> rettype = call_inst->typ;
+                    string fname = "iasm_" + iasm_prefix + std::to_string(iasm_count);
+                    string old_name = fname;
+                    string constraints = asm_func->constraints;
+                    shared_ptr<IRType> rettype = call_inst->typ;
 
-                                        vector<shared_ptr<FuncArg>> args;
-                                }
-                        } else if (dynamic_cast<IIf*>(i.get())) {
-                                auto if_inst = dynamic_cast<IIf*>(i.get());
-
-                                failed += find_inline_asm(*if_inst->true_body);
-                                failed += find_inline_asm(*if_inst->false_body);
-                        } else if (dynamic_cast<ILoop*>(i.get())) {
-                                auto loop_inst = dynamic_cast<ILoop*>(i.get());
-
-                                failed += find_inline_asm(*loop_inst->body);
-                        }
+                    vector<shared_ptr<FuncArg>> args;
                 }
+            } else if (dynamic_cast<IIf*>(i.get())) {
+                auto if_inst = dynamic_cast<IIf*>(i.get());
 
-                return failed;
-        };
+                failed += find_inline_asm(*if_inst->true_body);
+                failed += find_inline_asm(*if_inst->false_body);
+            } else if (dynamic_cast<ILoop*>(i.get())) {
+                auto loop_inst = dynamic_cast<ILoop*>(i.get());
+
+                failed += find_inline_asm(*loop_inst->body);
+            }
+        }
+
+        return failed;
+    };
 
 }
 
