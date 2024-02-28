72dcc499485b57789aac7a0ea4e2df868b086b45
xiaoqiangheye <wq2167@columbia.edu>
2024-02-28 15:35:21 -0500

    update

--- SpoqRules/SpoqRules.v
+++ SpoqRules/SpoqRules.v
@@ -152,41 +152,151 @@ End if_condition.
 
 (* eliminate match is not expressable *)
 
-
 (* Test refinement *)
-Record RData := mkRData {
-                    a : Z;
-                    b : Z
-                  }.
+(* Record RData := mkRData { *)
+(*                     a : Z; *)
+(*                     b : Z *)
+(*                   }. *)
 
 
-Definition low_spec_test (st: RData) :=
-  mkRData (a st) ((a st) + 1).
+(* Definition low_spec_test (v:Z) (st: RData) := *)
+(*   (v + 1, mkRData (a st) ((a st) + v)). *)
 
-Parameter oracle : RData -> RData.
+(* Parameter oracle : Z -> RData -> (Z * RData). *)
 
-Definition high_spec_test (st: RData) :=
-  (oracle st).
+(* Definition high_spec_test (v: Z) (st: RData) := *)
+(*   (oracle v st). *)
 
-Inductive refrel : RData -> RData -> Prop :=
-| hiding : forall hst lst, refrel hst lst -> refrel (oracle hst) {| a := a lst; b := a lst + 1 |}
-| normal : forall lst hst, hst = lst -> refrel hst lst.
-
-Lemma refinement :
-  forall hst hst' lst,
-    refrel hst lst ->
-    high_spec_test hst = hst' ->
-    exists lst',
-    low_spec_test lst = lst' /\ refrel hst' lst'.
-Proof.
-  intros.
-  unfold low_spec_test, high_spec_test in *.
-  exists {| a := a lst; b := a lst + 1 |}.
-  split. reflexivity. rewrite <- H0. apply hiding. apply H.
-Qed.
+(* Inductive refrel_v : Z -> Z -> Prop := *)
+(* | hiding_Z : forall hv lv hst , refrel_v hv lv -> refrel_v (fst (oracle hv hst)) (lv + 1) *)
+(* | normal_Z : forall v v', v = v' -> refrel_v v v'. *)
 
 
+(* Inductive refrel : RData -> RData -> Prop := *)
+(* | hiding : forall hst lst hv lv, refrel_v hv lv -> refrel hst lst -> refrel (snd (oracle hv hst)) {| a := a lst; b := a lst + lv |} *)
+(* | normal : forall lst hst, hst = lst -> refrel hst lst. *)
 
 
+(* Lemma refinement : *)
+(*   forall hst hst' lst hv lv hv', *)
+(*     refrel hst lst -> *)
+(*     refrel_v hv lv ->  *)
+(*     high_spec_test hv hst = (hv',hst') -> *)
+(*     exists lst' lv', *)
+(*     low_spec_test lv lst = (lv',lst') /\ refrel hst' lst' /\ refrel_v hv' lv'. *)
+(* Proof. *)
+(*   intros. *)
+(*   unfold low_spec_test, high_spec_test in *. *)
+(*   exists {| a := a lst; b := a lst + lv |}. exists (lv + 1). *)
+(*   split. reflexivity. *)
+(*   assert ((fst (oracle hv hst)) = hv'). rewrite H1. *)
+(*   simpl. reflexivity. *)
+(*   assert ((snd (oracle hv hst)) = hst'). rewrite H1. *)
+(*   simpl. reflexivity. *)
+(*   split. *)
+(*   rewrite <- H3. *)
+(*   apply hiding. apply H0. apply H. *)
+(*   rewrite <- H2. *)
+(*   apply hiding_Z. apply H0. *)
+(* Qed. *)
 
 
+(* (* Copied from metacoq tutorial *) *)
+
+(* From MetaCoq.Template Require Export All Checker Reduction. *)
+
+(* Notation "'$quote' x" := ltac:((let p y := exact y in *)
+(*                              quote_term *)
+(*                              x *)
+(*                              p)) (at level 0). *)
+
+(* Notation "'$run' f" := ltac:(let p y := exact y in *)
+(*                              run_template_program *)
+(*                              f *)
+(*                              p) (at level 0). *)
+
+(* Notation "'$quote_rec' x" := ($run (tmQuoteRec x)) (at level 0). *)
+
+(* Notation "'$unquote' x" := ltac:((let p y := match y with *)
+(*                                                existT_typed_term ?T ?b => exact b *)
+(*                                              end in *)
+(*                              run_template_program *)
+(*                              (tmUnquote x) *)
+(*                              p)) (at level 0). *)
+
+(* Notation "'$unquote' x : T" := ($run (tmUnquoteTyped T x)) (at level 0, T at level 100, x at next level). *)
+
+(* Definition unfold_toplevel {A} (x : A) := *)
+(*   tmBind (tmQuote x) (fun y => *)
+(*                         match y with *)
+(*                         | tConst na _ => *)
+(*                             tmEval (unfold na) x *)
+(*                         | y => tmReturn x *)
+(*                         end). *)
+
+(* Notation "'$Quote' x" := ($run (tmBind (unfold_toplevel x) (tmQuote))) (at level 0). *)
+
+(* Definition term_eqb (t1 t2 : term) := *)
+(*   @eq_term config.default_checker_flags init_graph t1 t2. *)
+
+(* Notation "t == u" := (term_eqb t u) (at level 70). *)
+
+(* Open Scope bs. *)
+(* Open Scope bool. *)
+(* Open Scope list. *)
+
+(* Definition tLam x A b := *)
+(*   tLambda {| binder_name := nNamed x; binder_relevance := Relevant |} A b. *)
+
+(* Definition tLet x A t b := *)
+(*   tLetIn {| binder_name := nNamed x; binder_relevance := Relevant |} t A b. *)
+
+(* Require Export Nat. *)
+
+(* Notation "'__'" := (hole) (no associativity, at level 0). *)
+
+(* From Coq Require Import List Utf8. *)
+
+(* Unset Guard Checking. *)
+
+
+(* Check ($quote (3 + 2)). *)
+
+
+(* Parameter X : Type. *)
+(* Parameter x : bool. *)
+(* Parameter y : list nat. *)
+(* Parameter f : nat -> nat. *)
+(* Check ($quote (if x then X else X)). *)
+(* Check ($quote (match y with *)
+(*                | a :: b :: [] => hd (a :: b :: [])                  *)
+(*                | [] => hd y *)
+(*                | c :: xs => hd y             *)
+(*                end)).             *)
+
+
+(* Print map_branch. *)
+
+(* Fixpoint match_subst (t : term) := *)
+(*   match t with *)
+(*   | tRel n => tRel n *)
+(*   | tVar id => tVar id *)
+(*   | tEvar ev args => tEvar ev (map match_subst args) *)
+(*   | tSort s => tSort s *)
+(*   | tCast t kind v => tCast (match_subst t) kind (match_subst v) *)
+(*   | tProd na ty body => tProd na (match_subst ty) (match_subst body) *)
+(*   | tLambda na ty body => tLambda na (match_subst ty) (let_to_lambda body) *)
+(*   | tLetIn na def def_ty body => tLetIn na (match_subst def) (match_subst def_ty) (match_subst body) *)
+(*   | tApp f args => tApp (match_subst f) (map match_subst args) *)
+(*   | tConst c u => tConst c u *)
+(*   | tInd ind u => tInd ind u *)
+(*   | tConstruct ind idx u => tConstruct ind idx u *)
+(*   | tCase ind p discr brs => *)
+(*       let brs' := map_branches (fun bbody => subs bbody discr ) brs in *)
+(*       tCase ind p' discr brs' *)
+(*   | tProj proj t => tProj proj (let_to_lambda t) *)
+(*   | tFix mfix idx => tFix (map (map_def let_to_lambda let_to_lambda) mfix) idx *)
+(*   | tCoFix mfix idx => tCoFix (map (map_def let_to_lambda let_to_lambda) mfix) idx *)
+(*   | tInt i => tInt i *)
+(*   | tFloat f => tFloat f *)
+(*   end. *)
