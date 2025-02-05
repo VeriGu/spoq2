0885c416c4ac6757a2db8a145cc56802b998c557
gxyang <ganxiang.yang@columbia.edu>
2025-02-05 02:13:42 -0500

    feat: smc_rec_destroy_spec_mem_data_inv

--- container/coq/revising/Security/rtt_map_data_inv.v
+++ container/coq/revising/Security/rtt_map_data_inv.v
@@ -11,6 +11,7 @@ Require Import Layer4.Spec.
 Require Import Layer5.Spec.
 Require Import Layer6.Spec.
 Require Import SecurityProof.
+Require Import walk_inv.
 (* Require Import SMCHandler.Spec. *)
 
 Local Open Scope string_scope.
@@ -31,6 +32,15 @@ Lemma lens_gpt_same:
   forall m st,
     (lens m st).(share).(gpt) = st.(share).(gpt).
 Admitted.
+
+Require Import Nat.
+Require Import PeanoNat.
+
+Lemma eqb_eq : forall n m : Z, (n =? m) = true -> n = m.
+Proof. apply Z.eqb_eq. Qed.
+
+Lemma eqb_neq : forall n m : Z, (n =? m) = false -> n <> m.
+Proof. apply Z.eqb_neq. Qed.
 
 Lemma lens_state_same:
   forall st v i,
@@ -63,6 +73,14 @@ Lemma rtt_same_cond1:
   (* same_rtt_structure st (st.[share].[granule_data] :< (st.(share).(granule_data) # v == d)). *)
   forall rtt_idx ipa_gidx,
     rtt_walk_abs st rtt_idx ipa_gidx = rtt_walk_abs (st.[share].[granule_data] :< (st.(share).(granule_data) # v == d)) rtt_idx ipa_gidx.
+Admitted.
+
+Lemma rtt_same_cond2: 
+  forall st v d
+  (Hirrelevant: (st.(share).(globals).(g_granules) @ v).(e_state_s_granule) <> GRANULE_STATE_RTT /\ (st.(share).(globals).(g_granules) @ v).(e_state_s_granule) <> GRANULE_STATE_DATA),
+  (* same_rtt_structure st (st.[share].[granule_data] :< (st.(share).(granule_data) # v == d)). *)
+  forall rtt_idx ipa_gidx,
+    rtt_walk_abs st rtt_idx ipa_gidx = rtt_walk_abs (st.[share].[globals].[g_granules] :< (st.(share).(globals).(g_granules) # v == d)) rtt_idx ipa_gidx.
 Admitted.
 
 Definition rtt_walk_as_same_def (st_0 st_1 : RData) : Prop :=
@@ -108,6 +126,20 @@ Lemma granule_unlock_spec_map_data_inv:
     rtt_map_data_inv ret_d.
 Admitted.
 
+
+Lemma lens_rtt_map_data_same: 
+  forall m st, 
+    rtt_map_data_inv st <-> rtt_map_data_inv (lens m st).
+Admitted.
+
+
+(* Abstracted-Pointer-specification for granule_unlock_spec *)
+(* Lemma granule_unlock_spec_pointer_preserve: 
+  forall d v_0 ret_d
+    (Hspec: granule_unlock_spec v_0 d = Some(ret_d))
+    (Hinv: rtt_map_data_inv d),
+    rec_to_rd_para *)
+
 (* collects all subspecs needed *)
 Ltac apply_subspec_invariants :=
   repeat match goal with
@@ -146,3 +178,159 @@ Proof with assumption.
   - apply_subspec_invariants.
   - apply_subspec_invariants.
 Qed.  
+
+
+Ltac intros_rd_para_state := 
+  match goal with 
+  | [H: context[rec_to_rd_para ?ptr ?x] |- _] => pose proof (rec_to_rd_para_state_rec ptr x) as Hrd; unfold GRANULE_STATE_RD in *
+  end. 
+  
+(* Ltac simpl_component :=
+  match goal with 
+  | [H: context[memset_spec _ _ _] |- _] => try apply memset_spec_walk_rev_lens in H 
+  | [H: context[granule_unlock_spec _ _] |- _] => try apply granule_unlock_spec_walk_rev_lens in H 
+  | [H: context[spinlock_acquire_spec _ _] |- _] => try apply spinlock_acquire_spec_walk_rev_lens in H 
+  | [H: context[spinlock_release_spec _ _] |- _] => try apply spinlock_release_spec_walk_rev_lens in H 
+  end. 
+
+Ltac simpl_walk_rev :=
+  try unfold GRANULE_STATE_RTT in *; try unfold GRANULE_STATE_RD in *;
+  try unfold GRANULE_STATE_REC in *; repeat simpl_component; try intros_rd_para_state. *)
+
+
+Lemma memset_spec_rtt_map_data_lens : 
+  forall st v_0 v_1 v_2 ret_n ret_st
+  (Hspec: memset_spec v_0 v_1 v_2 st = Some(ret_n, ret_st)),
+  ret_st = lens len_para st.
+Admitted.
+
+Lemma granule_unlock_spec_rtt_map_data_lens : 
+  forall v_0 st ret_st
+  (Hspec: granule_unlock_spec v_0 st = Some ret_st),
+  ret_st = lens len_para st.
+Admitted.
+
+Axiom rtt_root_state : 
+  forall st addr, 
+    rtt_is_root addr = true -> 
+    (st.(share).(globals).(g_granules) @ addr).(e_state_s_granule) = GRANULE_STATE_RTT.
+
+Lemma lens_rtt_walk_abs_same: 
+  forall m st rtt_idx ipa_gidx,
+    rtt_walk_abs st rtt_idx ipa_gidx = rtt_walk_abs (lens m st) rtt_idx ipa_gidx.
+Admitted.
+
+(* Axiom rec_to_rd_para_get_offset: 
+  forall ptr st,
+    (rec_to_rd_para ptr st).(poffset) = ptr.(poffset). *)
+
+Lemma rec_to_rd_para_offset_neq:
+forall st rd,
+  (rec_to_rd_para rd st).(poffset) <> rd.(poffset).
+Admitted.
+
+Lemma set_get_simpl1: 
+  forall st idx d, 
+    (st.(share).(globals).(g_granules) # idx == d) @ idx = d.
+Admitted.
+    (* Hrtt: ((g_granules (globals (share r0))) # rtt_idx == (((g_granules (globals (share r0))) @ rtt_idx).[e_state_s_granule]:< 1)) @ rtt_idx =  *)
+
+Lemma smc_rec_destroy_spec_mem_data_inv: 
+  forall v_0 st ret_n ret_st
+    (Hspec: smc_rec_destroy_spec v_0 st = Some(ret_n, ret_st))
+    (Hinv: rtt_map_data_inv st),
+    rtt_map_data_inv ret_st.
+Proof.
+  intros. unfold smc_rec_destroy_spec in Hspec.
+  autounfold with sem in *.
+  repeat simpl_hyp Hspec; try unfold GRANULE_STATE_DATA in *; try unfold GRANULE_STATE_ANY in *; try assumption; inv Hspec.
+  apply spinlock_acquire_spec_map_data_inv in C0; try assumption. 
+  - pose proof C0 as Hinv_r.
+    (* r -> (memset) -> r0 -> update state -> granule_unlock -> r1 *)
+    apply (lens_rtt_map_data_same len_para r) in C0.
+    apply memset_spec_rtt_map_data_lens in C4. rewrite <- C4 in *.
+    apply granule_unlock_spec_rtt_map_data_lens in C6.  
+    (* backward proof start *)
+    remember (r0.[share].[globals].[g_granules] :< ((g_granules (globals (share r0))) # meta_granule_offset (test_PA v_0) / 4096 ==
+    (((g_granules (globals (share r0))) @ (meta_granule_offset (test_PA v_0) / 4096)).[e_state_s_granule]:< 1))) as st'.
+    remember (meta_granule_offset (test_PA v_0) / 4096) as goal_idx.
+    {
+      assert (rtt_map_data_inv st').
+      {
+        unfold rtt_map_data_inv; try unfold GRANULE_STATE_RTT, GRANULE_STATE_ANY, GRANULE_STATE_DATA in *; 
+        intros rtt_idx ipa_gidx data_gidx Hrtt_is_root Hrtt Hwalk.
+        assert (rtt_walk_abs st' rtt_idx ipa_gidx = rtt_walk_abs r0 rtt_idx ipa_gidx) as Heq_rtt_abs.
+        { 
+          rewrite Heqst'. symmetry. eapply rtt_same_cond2. 
+          pose proof (lens_state_same r len_para goal_idx). rewrite <- C4 in H. rewrite H.
+          split; unfold not; intros; rewrite H0 in C1; inv C1.
+        }
+        assert ((st'.(share).(globals).(g_granules) @ rtt_idx).(e_state_s_granule) = (r.(share).(globals).(g_granules) @ rtt_idx).(e_state_s_granule)).
+        { 
+          rewrite Heqst'. simpl. destruct_zmap; subst. 
+          apply (rtt_root_state r) in Hrtt_is_root. rewrite Hrtt_is_root in *. inv C1. 
+          pose proof (lens_state_same r len_para rtt_idx). rewrite H. reflexivity. 
+        }
+        rewrite Heqst'. simpl in *. destruct_zmap; simpl;
+        unfold rtt_map_data_inv in Hinv_r;
+        rewrite Heq_rtt_abs in Hwalk; rewrite H in Hrtt; 
+        pose proof (lens_rtt_walk_abs_same len_para r rtt_idx ipa_gidx) as Heq_rtt_abs0; rewrite <- C4 in Heq_rtt_abs0; rewrite <- Heq_rtt_abs0 in Hwalk;
+        pose proof (Hinv_r rtt_idx ipa_gidx data_gidx Hrtt_is_root Hrtt Hwalk).
+        + destruct H0; subst data_gidx goal_idx; rewrite H0 in C1; inv C1.
+        + pose proof (lens_state_same r len_para data_gidx). subst r0. rewrite H1. auto.
+      }
+      assert (rtt_map_data_inv r1).
+      { pose proof (lens_rtt_map_data_same len_para st') as Heqv_st'. rewrite <- C6 in Heqv_st'. apply Heqv_st'. assumption. }
+      unfold rtt_map_data_inv; try unfold GRANULE_STATE_RTT, GRANULE_STATE_ANY, GRANULE_STATE_DATA in *.
+      (* intros rtt_idx ipa_gidx data_gidx Hrtt_is_root Hrtt Hwalk; simpl in *. *)
+        intros rtt_idx ipa_gidx data_gidx Hrtt_is_root; simpl in *.
+      (* rtt_idx will not be updated entry *)
+      destruct_zmap; simpl in *.
+      + pose proof (rec_to_rd_para_state_rec {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) as Hrd.
+        rewrite <- Heq in *.
+        apply (rtt_root_state r) in Hrtt_is_root. rewrite Hrtt_is_root in Hrd. inv Hrd. 
+      + intros.
+        pose proof (rec_to_rd_para_state_rec {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) as Hrd.
+        remember (poffset (rec_to_rd_para {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) / 4096) as rd_idx.
+        remember (r1.[share].[globals].[g_granules]
+        :< ((g_granules (globals (share r1))) # poffset (rec_to_rd_para {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) / 4096 ==
+            (((g_granules (globals (share r1))) @
+              (poffset (rec_to_rd_para {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) / 4096)).[e_ref]
+             :< ((e_ref
+                    (g_granules (globals (share r1))) @
+                    (poffset (rec_to_rd_para {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) / 4096)).[e_u_anon_3_0]
+                 :< (e_u_anon_3_0
+                       (e_ref
+                          (g_granules (globals (share r1))) @
+                          (poffset (rec_to_rd_para {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r) / 4096)) + -1))))) as st''.
+        assert (rtt_walk_abs st'' rtt_idx ipa_gidx = rtt_walk_abs r1 rtt_idx ipa_gidx) as Heq_rtt_abs.
+        {
+          rewrite Heqst''. symmetry. eapply rtt_same_cond2;
+          remember (meta_granule_offset (test_PA v_0) / 4096) as rec_idx. destruct (rd_idx =? rec_idx) eqn:Hrd_rec.
+          - split; unfold not; intros; rewrite Heqgoal_idx in *. 
+            pose proof (lens_state_same st' len_para rd_idx). subst goal_idx. rewrite C6 in H1.
+            rewrite <- Heqrd_idx in H1. rewrite H1 in H2. symmetry in H2. 
+            pose proof (rec_to_rd_para_state_rec {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r). rewrite <- Heqrd_idx in *. 
+            rewrite Heqst' in H2. simpl in H2.
+            apply eqb_eq in Hrd_rec. rewrite Hrd_rec in H3. rewrite H3 in C1. inv C1.
+            pose proof (lens_state_same st' len_para rd_idx). subst goal_idx. rewrite C6 in H1.
+            rewrite <- Heqrd_idx in H1. rewrite H1 in H2. symmetry in H2. 
+            pose proof (rec_to_rd_para_state_rec {| pbase := "granule_data"; poffset := meta_granule_offset (test_PA v_0) |} r). rewrite <- Heqrd_idx in *. 
+            rewrite Heqst' in H2. simpl in H2.
+            apply eqb_eq in Hrd_rec. rewrite Hrd_rec in H3. rewrite H3 in C1. inv C1.
+          - apply eqb_neq in Hrd_rec. rewrite <- Heqrd_idx.
+            pose proof (lens_state_same st' len_para rd_idx). rewrite <- C6 in H1. rewrite Heqst' in H1. rewrite H1. simpl in *. destruct_zmap. 
+            rewrite Heq0 in Hrd_rec. contra. 
+            pose proof (lens_state_same r len_para rd_idx). rewrite C4, H2. 
+            split; unfold not; intros; rewrite Hrd in H3; inv H3.
+        }
+        rewrite <- Heqrd_idx in Heqst''. rewrite <- Heqst'' in Hwalk. rewrite Heq_rtt_abs in Hwalk.
+        destruct_zmap; simpl in *;
+        unfold rtt_map_data_inv in H0; pose proof (H0 rtt_idx ipa_gidx data_gidx Hrtt_is_root Hrtt Hwalk); try assumption.
+        rewrite Heq0 in H1; assumption.
+    }
+  - apply_subspec_invariants.
+  - apply_subspec_invariants.
+  - apply_subspec_invariants.
+  - apply_subspec_invariants.
+Qed.
